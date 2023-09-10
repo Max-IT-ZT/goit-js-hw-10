16f394bd4f85187925d14d893240cd2e90cc164d
Max-IT-ZT <babuskomax@gmail.com>
2023-09-10 20:46:36 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const breedSelect = document.querySelector('.breed-select');
+  const catInfoDiv = document.querySelector('.cat-info');
+  const loader = document.querySelector('.loader');
+  const errorElement = document.querySelector('.error');
+
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      console.error('Fetch breeds error:', error);
+      errorElement.classList.remove('hidden');
+    });
+
+  breedSelect.addEventListener('change', () => {
+    const selectedBreedId = breedSelect.value;
+
+    if (selectedBreedId) {
+      loader.classList.remove('hidden');
+      catInfoDiv.classList.add('hidden');
+      errorElement.classList.add('hidden');
+
+      fetchCatByBreed(selectedBreedId)
+        .then(catData => {
+          const catInfoHTML = `
+          <div class="img-size"><img src="${catData[0].url}" alt="Cat Image"></div><div class="text">
+          <h1>${catData[0].breeds[0].name}</h1>
+          <p>${catData[0].breeds[0].description}</p>
+          <p><span class="temperament">Temperament:</span> ${catData[0].breeds[0].temperament}</p></div>
+        `;
+          console.log(catData);
+          catInfoDiv.innerHTML = catInfoHTML;
+        })
+        .catch(error => {
+          console.error('Fetch cat error:', error);
+          errorElement.classList.remove('hidden');
+        })
+        .finally(() => {
+          loader.classList.add('hidden');
+
+          catInfoDiv.classList.remove('hidden');
+        });
+    }
+  });
+});
